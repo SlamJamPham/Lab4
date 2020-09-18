f0f90a8eb1aaec7a57900deb15bda3e835e823fa
SlamJamPham <phillipphm@tamu.edu>
2020-09-18 16:15:34 -0500

    Hi

---
+++ db/migrate/20200915225639_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :author
+      t.string :genre
+      t.float :price
+      t.date :publishedDate
+
+      t.timestamps
+    end
+  end
+end
